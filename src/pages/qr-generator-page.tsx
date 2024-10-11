e5b971dbe082c9c676645099cdf231e6b0e35f1a
madliani <madliani@hotmail.com>
2024-10-11 18:20:16 +0400

    feat: qt code generator page

---
+++ src/pages/qr-generator-page.tsx
@@ -0,0 +1,15 @@
+import QrCodeGenerator from "@/components/QrCodeGenerator";
+import MainLayout from "@/layouts/MainLayout";
+
+export default () => {
+  return (
+    <MainLayout>
+      <div className="container mx-auto" id="tools">
+        <h2 className="text-4xl font-bold text-center mb-12">
+          QR Code Generator
+        </h2>
+        <QrCodeGenerator />
+      </div>
+    </MainLayout>
+  );
+};
